ca2225ad008484c41acdafcf22fbb1b908fe2dd3
Brittny Rosenow <brittnyrosenow@gmail.com>
2020-08-04 11:22:47 -0600

    updated server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-ar express = require('express');
+  
+var express = require('express');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var path = require('path')
